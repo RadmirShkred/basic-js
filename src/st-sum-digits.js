af36c70204bdb0beb453ec6f816092d863df4352
RadmirShkred <psitecton@gmail.com>
2021-09-12 01:28:29 +0300

    feat: add decision task st-sum-digits

--- src/st-sum-digits.js
+++ src/st-sum-digits.js
@@ -1,4 +1,4 @@
-import { NotImplementedError } from '../extensions/index.js';
+import {NotImplementedError} from '../extensions/index.js';
 
 /**
  * Given a number, replace this number with
@@ -12,7 +12,14 @@ import { NotImplementedError } from '../extensions/index.js';
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-export default function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getSumOfDigits(n) {
+  let mediumResult = n.toString().split('');
+  let sum = 0;
+  for (let digit of mediumResult) {
+    sum += +digit;
+  }
+  if (sum.toString().length > 1) {
+    sum = getSumOfDigits(sum);
+  }
+  return sum;
 }
