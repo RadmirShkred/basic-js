41a9c6d167ad58e06911837cbac1d39836f0c0c8
RadmirShkred <psitecton@gmail.com>
2021-09-15 21:30:47 +0300

    feat: add decision task st-sort-by-height

--- src/st-sort-by-height.js
+++ src/st-sort-by-height.js
@@ -1,4 +1,4 @@
-import { NotImplementedError } from '../extensions/index.js';
+import {NotImplementedError} from '../extensions/index.js';
 
 /**
  * Given an array with heights, sort them except if the value is -1.
@@ -11,7 +11,17 @@ import { NotImplementedError } from '../extensions/index.js';
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-export default function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function sortByHeight(arr) {
+  if (!arr.includes(-1)) {
+    return arr.sort((a, b) => a - b);
+  }
+  let newArray = [...arr];
+  newArray = newArray.filter(elem => elem !== -1).sort((a, b) => a - b);
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== -1) {
+      arr[i] = newArray[0];
+      newArray.shift();
+    }
+  }
+  return arr;
 }
