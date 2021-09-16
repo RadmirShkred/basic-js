9d6afe538109033738f8932206a05690b2524c27
RadmirShkred <psitecton@gmail.com>
2021-09-16 13:30:44 +0300

    feat: add decision task st-mine-sweeper

--- src/st-mine-sweeper.js
+++ src/st-mine-sweeper.js
@@ -1,4 +1,4 @@
-import { NotImplementedError } from '../extensions/index.js';
+import {NotImplementedError} from '../extensions/index.js';
 
 /**
  * In the popular Minesweeper game you have a board with some mines and those cells
@@ -23,7 +23,34 @@ import { NotImplementedError } from '../extensions/index.js';
  *  [1, 1, 1]
  * ]
  */
-export default function minesweeper (/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function minesweeper(matrix) {
+  let matrixLength = matrix.length;
+  let subMatrixLength = matrix[0].length;
+  let newArray = [[]];
+  for (let i = 0; i < matrixLength; i++) {
+    newArray.push([]);
+    for (let j = 0; j < subMatrixLength; j++) {
+      newArray[i][j] = 0;
+    }
+  }
+  newArray.pop();
+  for (let i = 0; i < matrixLength; i++) {
+    for (let j = 0; j < subMatrixLength; j++) {
+      let count = 0;
+      if (Array.isArray(matrix[i - 1])) {
+        if (matrix[i - 1][j - 1]) count += 1;
+        if (matrix[i - 1][j]) count += 1;
+        if (matrix[i - 1][j + 1]) count += 1;
+      }
+      if (Array.isArray(matrix[i + 1])) {
+        if (matrix[i + 1][j - 1]) count += 1;
+        if (matrix[i + 1][j]) count += 1;
+        if (matrix[i + 1][j + 1]) count += 1;
+      }
+      if (matrix[i][j - 1]) count += 1;
+      if (matrix[i][j + 1]) count += 1;
+      newArray[i][j] = count;
+    }
+  }
+  return newArray;
 }
