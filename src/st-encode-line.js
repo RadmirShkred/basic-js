a79d95f51502216a3ac52536df855bc42aec5d0c
RadmirShkred <psitecton@gmail.com>
2021-09-17 20:42:51 +0300

    fix: fix decision task st-encode-line

--- src/st-encode-line.js
+++ src/st-encode-line.js
@@ -1,4 +1,4 @@
-import { NotImplementedError } from '../extensions/index.js';
+import {NotImplementedError} from '../extensions/index.js';
 
 /**
  * Given a string, return its encoding version.
@@ -10,7 +10,22 @@ import { NotImplementedError } from '../extensions/index.js';
  * For aabbbc should return 2a3bc
  *
  */
-export default function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function encodeLine(str) {
+  if (str === '') return str;
+  const res = [];
+  let count = 1;
+  const arr = str.split('');
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === arr[i + 1]) {
+      count += 1;
+    } else {
+      if (count === 1) {
+        res.push(arr[i]);
+      } else {
+        res.push(`${count}${arr[i]}`);
+      }
+      count = 1;
+    }
+  }
+  return res.join('');
 }
