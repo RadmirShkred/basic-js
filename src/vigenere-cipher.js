901be950afe0eb36d88d34e0b1ce4c0fbb001e1f
RadmirShkred <psitecton@gmail.com>
2021-09-30 15:40:42 +0300

    feat: add decision vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,31 +1,103 @@
-import { NotImplementedError } from '../extensions/index.js';
+import {NotImplementedError} from '../extensions/index.js';
 
 /**
  * Implement class VigenereCipheringMachine that allows us to create
  * direct and reverse ciphering machines according to task description
- * 
+ *
  * @example
- * 
+ *
  * const directMachine = new VigenereCipheringMachine();
- * 
+ *
  * const reverseMachine = new VigenereCipheringMachine(false);
- * 
+ *
  * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
- * 
+ *
  * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
- * 
+ *
  * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
- * 
+ *
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
- * 
+ *
  */
 export default class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(reverse) {
+    this.reverse = reverse;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(string, keyWord) {
+    if (!string || !keyWord) {
+      throw Error("Incorrect arguments!");
+    }
+
+    let str = string.toUpperCase();
+    let key = keyWord.toUpperCase();
+    let resultArr = [];
+    let resultStr = '';
+    let keyPosition = 0;
+
+    for (let i = 0; i < str.length; i++) {
+      if (str[i].charCodeAt() < 65 || str[i].charCodeAt() > 90) {
+        resultArr.push(str[i]);
+        continue;
+      }
+      if (key[keyPosition].charCodeAt() + (str[i].charCodeAt() - 65) <= 90) {
+        resultArr.push(key[keyPosition].charCodeAt() + (str[i].charCodeAt() - 65));
+      }
+      if (key[keyPosition].charCodeAt() + (str[i].charCodeAt() - 65) > 90) {
+        resultArr.push((key[keyPosition].charCodeAt() + (str[i].charCodeAt() - 65)) - 90 + 64);
+      }
+      keyPosition === key.length - 1 ? keyPosition = 0 : keyPosition++;
+    }
+
+    for (let i = 0; i < resultArr.length; i++) {
+      let letter = String.fromCharCode(resultArr[i]);
+      if (typeof (resultArr[i]) !== 'number') {
+        resultStr += resultArr[i];
+        continue;
+      }
+      resultStr += letter;
+    }
+    if (this.reverse === false) {
+      resultStr = resultStr.split('').reverse().join('');
+    }
+    return resultStr;
+  }
+
+  decrypt(string, keyWord) {
+    if (!string || !keyWord) {
+      throw Error("Incorrect arguments!");
+    }
+    let str = string.toUpperCase();
+    let key = keyWord.toUpperCase();
+    let resultArr = [];
+    let resultStr = '';
+    let keyPosition = 0;
+
+    for (let i = 0; i < str.length; i++) {
+      if (str[i].charCodeAt() < 65 || str[i].charCodeAt() > 90) {
+        resultArr.push(str[i]);
+        continue;
+      }
+      if ((str[i].charCodeAt() + 65 - key[keyPosition].charCodeAt()) > 64) {
+        resultArr.push(str[i].charCodeAt() + 65 - key[keyPosition].charCodeAt());
+      }
+      if (str[i].charCodeAt() + 65 - key[keyPosition].charCodeAt() < 65) {
+        resultArr.push(91 - (key[keyPosition].charCodeAt() - str[i].charCodeAt()));
+      }
+      keyPosition === key.length - 1 ? keyPosition = 0 : keyPosition++;
+    }
+
+    for (let i = 0; i < resultArr.length; i++) {
+      let letter = String.fromCharCode(resultArr[i]);
+      if (typeof (resultArr[i]) !== 'number') {
+        resultStr += resultArr[i];
+        continue;
+      }
+      resultStr += letter;
+    }
+    if (this.reverse === false) {
+      resultStr = resultStr.split('').reverse().join('');
+    }
+    return resultStr;
   }
 }
